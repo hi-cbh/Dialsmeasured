aadfe7f557d7c47997028e37c0f33d5b807f173c
apple <1>
2018-05-09 12:37:28 +0800

    测试

--- src/mail/sendEmailSmtp.py
+++ src/mail/sendEmailSmtp.py
@@ -10,7 +10,8 @@ from email.utils import parseaddr, formataddr
 #单个测试邮件
 tester = "13533348571@139.com"
 # 多个联系人
-mamEmail='13533348571@139.com,18022340679@139.com,13790383896@139.com'
+# mamEmail='13533348571@139.com,18022340679@139.com,13790383896@139.com'
+mamEmail=''
 # 发给其他人的
 mamEmailOther='13533348571@139.com,18022340679@139.com,13790383896@139.com,13802883234@139.com,wenyaoneng@139.com,18933905900@139.com,13610128827@139.com,13580491687@163.com'
 
