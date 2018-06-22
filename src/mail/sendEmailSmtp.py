767c9f626d7e364520903d2f44004aa70de834f4
apple <1>
2018-06-22 14:59:48 +0800

    更新ssl发送方式

--- src/mail/sendEmailSmtp.py
+++ src/mail/sendEmailSmtp.py
@@ -252,6 +252,47 @@ class SendMail():
 
         try:
             s = smtplib.SMTP()
+            # s.connect(smtp_server, "25")
+            s.connect(smtp_server, "465")
+            s.login(from_mail, mail_pass)
+            s.sendmail(from_mail, areceiver.split(','), msg.as_string())
+            s.quit()
+            print("发送成功")
+        except smtplib.SMTPException as e:
+            print("Error: %s" % e)
+            return False
+        else:
+            return True
+
+    def send_mail_str_163_ssl(self, subject, message="", is_test=False):
+        '''发送邮件，固定格式'''
+        smtp_server = 'smtp.163.com'
+        from_mail = self.username + '@163.com'
+        mail_pass = self.pwd
+
+        if is_test:
+            # areceiver = 'hi_cbh@qq.com'
+            areceiver = '13501538531@139.com,hi_cbh@qq.com'
+        else:
+            # areceiver = "hi_cbh@qq.com,wujun11121@163.com"
+            areceiver = "hi_cbh@qq.com"
+
+
+        body = []
+        body.append(message)
+        body=''.join(body)
+
+        # print("邮件正式发送内容： %s" %body)
+        print('邮件正式发送')
+
+        msg = MIMEText(body, 'html', 'utf-8')
+        # Header对中文进行转码
+        msg['From'] = self._format_addr(u"拨测账号 <%s>" % from_mail)
+        msg['To'] = areceiver
+        msg['Subject'] = Header(subject, 'utf-8')
+
+        try:
+            s = smtplib.SMTP_SSL()
             s.connect(smtp_server, "25")
             s.login(from_mail, mail_pass)
             s.sendmail(from_mail, areceiver.split(','), msg.as_string())
@@ -262,6 +303,7 @@ class SendMail():
             return False
         else:
             return True
+
 
     def send_mail_out_163(self, subject, message=[], is_test=False):
         '''发送邮件，固定格式，发送移动'''
@@ -318,6 +360,6 @@ if __name__ == "__main__":
         # s.sendMail('testEmail','Python 邮件发送测试...')
 
         line = ["testemail"]
-        s.send_mail_str_163("testEmail"," 邮件发送测试",is_test=True)
+        s.send_mail_str_163_ssl("testEmail"," 邮件发送测试",is_test=True)
         # s.send_mail_out_163("testEmail",["邮件发送测试","test2"],is_test=True)
         time.sleep(5)
